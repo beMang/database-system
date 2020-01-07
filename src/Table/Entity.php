676fc5765f5d5fae32458f07af3ca2131a434d91
beMang <antonuttiadrien@gmail.com>
2020-01-07 20:55:08 +0100

    feat(entity) generation of classes of entities
    * generation of classes of entities
    * tests the generator
    * added TableException type

--- src/Table/Entity.php
+++ src/Table/Entity.php
@@ -4,9 +4,4 @@ namespace bemang\Database\Table;
 
 abstract class Entity
 {
-    //TODO : mettre dans le générateur
-    public function getClassName(): string
-    {
-        return __CLASS__;
-    }
 }
--- src/Table/EntityGenerator.php
+++ src/Table/EntityGenerator.php
@@ -6,24 +6,32 @@ use bemang\Database\Manager\DBManager;
 
 class EntityGenerator
 {
-    public static function generate(string $tableName, string $databaseName, DBManager $manager): void
+    public static function generate(string $tableName, string $databaseName, DBManager $manager): string
     {
         $fields = EntityGenerator::getFieldsName($tableName, $databaseName, $manager);
 
-        $parts = ['namespace bemang\Database\Table;'];
-        $parts[] = 'class ' . $tableName . ' extends Entity{';
+        if (empty($fields)) {
+            throw new \bemang\Database\Exceptions\TableException('La table n\'existe pas dans la base de donnée');
+        }
+
+        if (class_exists('\bemang\\Database\\Table\Entities\\' . $tableName)) {
+            throw new \bemang\Database\Exceptions\TableException(
+                'La classe de cette table a déjà été générée'
+            );
+        }
+
+        $parts = ['namespace bemang\Database\Table\Entities;'];
+        $parts[] = 'class ' . $tableName . ' extends \bemang\Database\Table\Entity{';
         $parts[] = self::generateAttributes($fields);
         $parts[] = self::generateMethods($fields);
         $parts[] = '}';
-
-        $code = join('', $parts);
-        var_dump($code);
-        //eval($code);
+        eval(join('', $parts));
+        return '\bemang\\Database\\Table\Entities\\' . $tableName;
     }
 
     protected static function getFieldsName(string $tableName, string $databaseName, DBManager $manager): array
     {
-        $query = 'select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME =  :table'; //TODO : vérifier que la table existe
+        $query = 'select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME =  :table';
         $queryResults = $manager->sql($query, $databaseName, ['table' => $tableName]);
         $results = [];
         foreach ($queryResults as $object) {
@@ -43,7 +51,11 @@ class EntityGenerator
 
     protected static function generateMethods(array $fieldsName): string
     {
-        $string = '';
-        return $string;
+        $methods = [];
+        foreach ($fieldsName as $field) {
+            $methods[] = 'public function get' . ucfirst(strtolower($field)) .
+            '(){return $this->' . $field . ';}';
+        }
+        return join('', $methods);
     }
 }
